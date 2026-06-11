64686a8022659a4460c25ba86cb5f523acc06853
Thanuj Basavarajugari <thanujbasavarajugarigmail@gmail.com>
2026-06-11 10:10:42 +0530

    fixed script syntax

--- DRIVE EASY CAR TRAVEL/script.js
+++ DRIVE EASY CAR TRAVEL/script.js
@@ -7,56 +7,51 @@ const SUPABASE_KEY =
 "sb_publishable_KIqnwBxwxww7Wn0j7O6SDQ_nTJldkWX";
 
 const supabaseClient = supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
 
 const bookingForm = document.getElementById("bookingForm");
 
 if (bookingForm) {
 
+    bookingForm.addEventListener("submit", async (e) => {
 
-bookingForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    e.preventDefault();
+        alert("SUBMIT CLICKED");
 
-    alert("SUBMIT CLICKED");
+        const booking = {
+            fullname: document.getElementById("fullname").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            pickup_date: document.getElementById("pickup_date").value,
+            pickup_location: document.getElementById("pickup_location").value,
+            drop_location: document.getElementById("drop_location").value,
+            passengers: document.getElementById("passengers").value,
+            car_name: document.getElementById("car_name").value
+        };
 
-    const booking = {
-        fullname: document.getElementById("fullname").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        pickup_date: document.getElementById("pickup_date").value,
-        pickup_location: document.getElementById("pickup_location").value,
-        drop_location: document.getElementById("drop_location").value,
-        passengers: document.getElementById("passengers").value,
-        car_name: document.getElementById("car_name").value
-    };
+        try {
 
-    try {
+            const { error } = await supabaseClient
+                .from("bookings")
+                .insert([booking]);
 
-        const { error } = await supabaseClient
-            .from("bookings")
-            .insert([booking]);
+            if (error) {
+                alert("Supabase Error: " + error.message);
+                return;
+            }
 
-        if (error) {
-            console.error(error);
-            alert("Supabase Error: " + error.message);
-            return;
+            alert("Booking Successful");
+            bookingForm.reset();
+
+        } catch (err) {
+
+            alert("Network Error: " + err.message);
+
         }
 
-        alert("Booking Successful");
+    });
 
-        bookingForm.reset();
-
-    } catch (err) {
-
-        console.error(err);
-
-        alert(
-            "Network Error: " +
-            err.message
-        );
-    }
-});
 }
